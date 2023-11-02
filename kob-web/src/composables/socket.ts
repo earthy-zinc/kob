1cffd97def4aa795b0b120097a18950818ca4c02
土味锌 <1066365803@qq.com>
2023-11-02 12:37:42 +0800

    新增更改

--- kob-web/src/composables/socket.ts
+++ kob-web/src/composables/socket.ts
@@ -2,7 +2,7 @@ import { Token } from '~/utils'
 
 const formatSocketUrl = () => {
   const urlPrefix = import.meta.env.VITE_BASE_SOCKET_URL
-  return `${urlPrefix}/websocket/${Token.get()}/`
+  return new URL(`/websocket/${Token.get()}/`, urlPrefix)
 }
 
 export function useSocket(onmessage: (msg: any) => void) {
